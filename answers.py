424a1e952570b5b3f506cca5f6b6fea1818ea5fc
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-03-27 19:47:44 +0200

    all variables are no longer hard coded.

--- answers.py
+++ answers.py
@@ -5,31 +5,40 @@ client = MongoClient('mongodb+srv://PythonApp:4X0LUM8zjS7Q@researchcluster.usrji
 db = client.research
 collection = db.answers
 
+var1 = 'gender'
+var2 = 'age'
+all_variables = [var1, var2]
+
 
 class Answers(Resource):
 
     def get(self):
         all_documents = collection.find()
         list_document = list(all_documents)
-        return {'data': list_document}, 200
+        # change each element's _id type from 'bson.objectid.ObjectId' to 'str' to be returned to the final json view
+        data = []
+        for document in list_document:
+            document['_id'] = str(document['_id'])
+            data.append(document)
+        return {'data': data}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
         parser.add_argument('_id', required=False)
-        parser.add_argument('gender', required=False)
-        parser.add_argument('age', required=False)
-
+        for var in all_variables:
+            parser.add_argument(var, required=False)
+        print("the first part")
         args = parser.parse_args()
 
         # creating new document
         new_data = {
-            '_id': args['_id'],
-            'gender': args['gender'],
-            'age': args['age']
+            '_id': args['_id']
         }
+        for var in all_variables:
+            new_data[var] = args[var]
         # adding new document
         collection.update_one(new_data, {'$set': new_data}, upsert=True)
-
+        print("the third part")
         # reading answers
         return self.get(), 200
 
