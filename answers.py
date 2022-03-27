51a0f9b2efbee0746a64e5ec008e8869181423f9
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-03-27 19:16:39 +0200

    The file is now ready to get data from MongoDB

--- answers.py
+++ answers.py
@@ -1,108 +1,56 @@
-import ast
-
 from flask_restful import Resource, reqparse
-import pandas as pd
+from pymongo import *
+
+client = MongoClient('mongodb+srv://PythonApp:4X0LUM8zjS7Q@researchcluster.usrji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.research
+collection = db.answers
+
 
 class Answers(Resource):
 
     def get(self):
-        data = pd.read_csv('answers.csv')
-        data = data.to_dict()
-        return {'data': data}, 200
+        all_documents = collection.find()
+        list_document = list(all_documents)
+        return {'data': list_document}, 200
 
     def post(self):
         parser = reqparse.RequestParser()
-
         parser.add_argument('_id', required=False)
         parser.add_argument('gender', required=False)
         parser.add_argument('age', required=False)
 
         args = parser.parse_args()
 
-#         creating new dataframe
-        new_data = pd.DataFrame({
+        # creating new document
+        new_data = {
             '_id': args['_id'],
             'gender': args['gender'],
-            'age': args['age'],
-            'locations':[[]]
-        },
-        index=[0])
+            'age': args['age']
+        }
+        # adding new document
+        collection.update_one(new_data, {'$set': new_data}, upsert=True)
 
-#       reading answers csv
-        data = pd.read_csv('answers.csv')
-
-        if args['_id'] in list(data['_id']):
-            return {
-                'message': f"'{args['_id']}' already exists."
-            }, 401
-        else:
-#           add the newly provided data
-            data = data.append(new_data, ignore_index=True)
-
-#           save back to csv
-            data.to_csv('answers.csv', index=False)
-
-            return {'data':data.to_dict()}, 200
-
-    def put(self):
-#         reading csv file
-        parser = reqparse.RequestParser()
-        parser.add_argument('_id', required=False)
-        parser.add_argument('location', required=False)
-        args = parser.parse_args()
-
-#         reading csv file
-        data = pd.read_csv('answers.csv')
-
-        if args['_id'] in list(data['_id']):
-#           evaluate strings of lists to lists
-
-            data['locations'] = data['locations'].apply(
-                lambda x: ast.literal_eval(x)
-            )
-#           select the users by id
-            user_data = data[data['_id'] == args['_id']]
-
-#           updating user's location
-            user_data['locations'] = user_data['locations'].values[0].append(args['location'])
-
-#           save back to csv
-            data.to_csv('answer.csv', index=False)
-
-#           return the data and 200 OK
-            return {'data': data.to_dict()}, 200
-        else:
-#             if the id does not exist
-            return {
-                'message': f"'{args['_id']}' user not found"
-            }, 404
+        # reading answers
+        return self.get(), 200
 
     def delete(self):
-
         parser = reqparse.RequestParser()
-        parser.add_argument('_id', required=False)
+        parser.add_argument('_id', required=True)
         args = parser.parse_args()
 
-        #         reading csv file
-        data = pd.read_csv('answers.csv')
+        # reading csv file
+        all_documents = collection.find()
+        list_document = list(all_documents)
 
-        if args['_id'] in list(data['_id']):
-            #           remove data entry
-            data = data[data['_id'] != args['_id']]
+        if collection.find_one({'_id': str(args['_id'])}):
+            # remove data entry
+            collection.delete_one({'_id': str(args['_id'])})
 
-            #           save back to csv
-            data.to_csv('answer.csv', index=False)
-
-            #           return the data and 200 OK
-            return {'data': data.to_dict()}, 200
+            # reading answers
+            return self.get(), 200
 
         else:
-            #             if the id does not exist
+            # if the id does not exist
             return {
                        'message': f"'{args['_id']}' user not found"
                    }, 404
-
-
-
-class Locations(Resource):
-    pass
--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from flask import Flask
-from flask_restful import Resource, Api, reqparse
-import pandas as pd
-import ast
-from answers import Answers, Locations
+from flask_restful import Api
 
+from answers import Answers
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Answers, '/answers')
-api.add_resource(Locations, '/locations')
 
 
 if __name__ == '__main__':
