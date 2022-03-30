32565b2bdaabaa3b2b274405714a69de78ecff39
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-03-30 14:54:53 +0200

    ID is no longer an input. Validation function has been created

--- answers.py
+++ answers.py
@@ -20,69 +20,76 @@ class Answers(Resource):
         for document in list_document:
             document['_id'] = str(document['_id'])
             data.append(document)
+        # data = map((self.change_to_string, list_document))
         return {'data': data}, 200
+
+    # def change_to_string(self, var):
+    #     dictionary: dict
+    #     dictionary[var] = str(dictionary[var])
+    #     return dictionary
+
+
+
+
 
     def post(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('_id', required=False)
         for var in all_variables:
             parser.add_argument(var, required=False)
         args = parser.parse_args()
 
+        self.validationInput(args[var2], var2)
+        self.validationInputGender(args[var1], var1)
+
         # creating new document
-        new_data = {
-            '_id': args['_id']
-        }
+        new_data = {}
         for var in all_variables:
             new_data[var] = args[var]
 
         # adding new document
-        if len(args['_id']) == 24:
-            if self.check_user_input(args[var2], var2) == 'number':
-                if self.check_user_input(args[var1], var1) == 'string':
-                    collection.update_one(new_data, {'$set': new_data}, upsert=True)
-                    return self.get(), 200
-                else:
-                    return {
-                               'message': f"'{args[var1]}' is not a valid input for {var1}"
-                           }, 404
-            else:
-            # reading answers
-                return {
-                               'message': f"'{args[var2]}' is not a valid input for {var2}"
-                           }, 404
-        else:
-            return {
-                               'message': f"'{args['_id']}' is not a valid ID, please make sure it is 24 characters long"
-                           }, 404
+
+        # collection.update_one(new_data, {'$set': new_data}, upsert=True)
+        # return self.get(), 200
+
 
     def delete(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('_id', required=True)
+        parser.add_argument(var1, required=True)
+        parser.add_argument(var2, required=True)
         args = parser.parse_args()
 
-        # reading csv file
-        all_documents = collection.find()
-        list_document = list(all_documents)
-
-        if collection.find_one({'_id': str(args['_id'])}):
+        if collection.find_one({var1: str(args[var1])}) and collection.find_one({var2: str(args[var2])}):
             # remove data entry
-            collection.delete_one({'_id': str(args['_id'])})
-
+            collection.delete_one({var1: str(args[var1])})
             # reading answers
             return self.get(), 200
 
         else:
             # if the id does not exist
             return {
-                       'message': f"'{args['_id']}' user not found"
+                       'message': f"'{args[var1]}', '{args[var2]}' not found"
                    }, 404
 
-    def check_user_input(self, user_input, classification):
-        try:
-            if int(user_input):
-                if 15 < int(user_input) < 90:
-                    return "number"
-        except:
-            if str(user_input) == 'male' or str(user_input) == 'female':
-                return "string"
+    def validationInput(self, argument, var_age):
+        if not hasattr(argument, var_age):
+            return {
+                       'message': f"'you cannot leave the {var_age} field empty"
+                   }, 404
+        if not argument.isnumeric(argument):
+            return {
+                       'message': f"'{argument}' is not a valid input for {var_age}"
+                   }, 401
+        if not 5 < argument < 100:
+            return {
+                       'message': f"'{argument}' is not a valid input for {var_age}, it must be between 5 and 100"
+                   }, 401
+
+    def validationInputGender(self, argument, var_gender):
+        if not hasattr(argument, var_gender):
+            return {
+                       'message': f"'you cannot leave the {var_gender} field empty"
+                   }, 401
+        if argument != 'female' or argument != 'male':
+            return {
+                       'message': f" {argument} is not a valid input for {var_gender}, must be 'male' or 'female'"
+                   }, 401
