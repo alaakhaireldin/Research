7ef84c3ba3dfa9f050ddd920d27d635d86747ccf
alaakhaireldin <alaa.khaireldin@gmail.com>
2022-04-12 16:18:22 +0200

    final fix, deleting unnecessary lines

--- answers.py
+++ answers.py
@@ -1,19 +1,14 @@
 from flask_restful import Resource, reqparse, inputs
 from pymongo import *
 
-client = MongoClient('mongodb+srv://PythonApp:4X0LUM8zjS7Q@researchcluster.usrji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://PythonApp:4X0LUM8zjS7Q@researchcluster.usrji.mongodb.net/'
+                     'myFirstDatabase?retryWrites=true&w=majority')
 db = client.research
 collection = db.answers
 
 var1 = 'gender'
 var2 = 'age'
 all_variables = [var1, var2]
-
-
-def validationInput(argument):
-    if not 5 < int(argument) < 100:
-        return f"'{argument}' is not a valid input for age, it must be between 5 and 100"
-    return None
 
 
 class Answers(Resource):
@@ -29,15 +24,13 @@ class Answers(Resource):
         # data = map((self.change_to_string, list_document))
         return {'data': data}, 200
 
-
-
-
-
     def post(self):
         parser = reqparse.RequestParser()
 
-        parser.add_argument(var1, type=str, choices=['female', 'male'], required=True, help='invalid input, gender must be male or female')
-        parser.add_argument(var2, type=int, required=True, help='invalid input, age must be number between 5 and 100')
+        parser.add_argument(var1, type=str, choices=['female', 'male'], required=True, help='invalid input, gender'
+                                                                                            ' must be male or female')
+        parser.add_argument(var2, type=int, required=True, choices=[range(5, 101)], help='invalid input, age must '
+                                                                                         'be number between 5 and 100')
 
         args = parser.parse_args()
 
@@ -46,20 +39,9 @@ class Answers(Resource):
         for var in all_variables:
             new_data[var] = args[var]
 
-        gender_error = validationInputGender(args[var1])
-        age_error = validationInput(args[var2])
-
-
-        if age_error:
-            return {'message': age_error}, 400
-
-        if gender_error:
-            return {'message': gender_error}, 400
-        # adding new document
         else:
             collection.update_one(new_data, {'$set': new_data}, upsert=True)
             return self.get(), 200
-
 
     def delete(self):
         parser = reqparse.RequestParser()
